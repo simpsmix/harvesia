336df89368e0564ac4bbf008aa2740e7c9906d10
simpsmix <rependis@gmail.com>
2016-05-03 11:48:46 +0700

    Added files via upload

---
+++ src/main/java/info/androidhive/loginandregistration/activity/AllProductActivity.java
@@ -0,0 +1,13 @@
+package info.androidhive.loginandregistration.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_product);
+    }
+}
